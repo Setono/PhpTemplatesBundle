a7bec4dcc4e76a15b555d5919c1700ce8c4f325d
Joachim Løvgaard <joachim@loevgaard.dk>
2020-05-15 13:24:34 +0200

    Removed the config component

--- src/DependencyInjection/SetonoPhpTemplatesExtension.php
+++ src/DependencyInjection/SetonoPhpTemplatesExtension.php
@@ -13,9 +13,7 @@ final class SetonoPhpTemplatesExtension extends Extension
 {
     public function load(array $config, ContainerBuilder $container): void
     {
-        $config = $this->processConfiguration($this->getConfiguration([], $container), $config);
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-
         $loader->load('services.xml');
     }
 }
