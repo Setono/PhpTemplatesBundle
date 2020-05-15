d9d5dd50687bcfaa7001602a0271b39d64bb759a
Joachim Løvgaard <joachim@loevgaard.dk>
2020-05-15 10:54:03 +0200

    Fix some build errors

--- src/DependencyInjection/SetonoPhpTemplatesExtension.php
+++ src/DependencyInjection/SetonoPhpTemplatesExtension.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Setono\PhpTemplatesBundle\DependencyInjection;
 
-use Exception;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
